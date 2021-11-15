6972aeea31fb5267194b04eee0e76b8b799cff09
Tegan Storm Friedenthal <55839174+teganstorm@users.noreply.github.com>
2021-11-15 15:00:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,27 @@
 # Fraser Steven (fs65)
 # Tegan Friedenthal (tf50)
 
+# part 7: GUI using tkinter
+filename = ""
+def openfile():
+    print("open file button pressed")
+    filename = E1.get()
+    if (filename == ""):
+        print("Error... No file name was entered")
+    else:
+        print(filename)
+
+from tkinter import *
+gui = Tk()
+gui.geometry("1200x600")
+L1 = Label(gui, text="File Name:")
+L1.pack()
+E1 = Entry(gui, bd =2)
+E1.pack(fill = X)
+B = Button(gui, text ="Analyse File", command = openfile)
+B.pack()
+gui.mainloop()
+
 # open and read specified file
 
 
@@ -23,4 +44,3 @@
 # part 6: also likes graph
 
 
-# part 7: GUI using tkinter
