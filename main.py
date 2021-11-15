b2c64fd6407e14f037889ba6601f568662954671
Tegan Storm Friedenthal <55839174+teganstorm@users.noreply.github.com>
2021-11-15 14:33:05 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# F20SC: Industrial Programming 
+# Coursework 2
+# Data Analysis of a Document Tracker
+
+# Fraser Steven (fs65)
+# Tegan Friedenthal (tf50)
+
+# open and read specified file
+
+
+# part 2: views by country/continent 
+
+
+# part 3: views by browser
+
+
+# part 4: reader profiles
+
+
+# part 5: also likes functionnality
+
+
+# part 6: also likes graph
+
+
+# part 7: GUI using tkinter
