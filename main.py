55779c013e0a9fb605e14d4d22b3a699d29dad00
Tegan Storm Friedenthal <55839174+teganstorm@users.noreply.github.com>
2021-11-22 15:56:28 +0400

    part 5 complete but not tested on large data yet

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ from tkinter import *
 
 # ------------Variables------------
 data = None
+filename = None
+E2 = None
+E3 = None
+L6 = None
+gui2 = None 
 
 # ------------part 2: views by country/continent------------
 # part a for countries 
@@ -121,39 +126,185 @@ def show_reader_profile_info():
 # ------------part 5: also likes functionnality------------
 # part a
 def get_readers_of_document(document_uuid):
-    readers_of_document = set() # set because set will not allow duplicates and a user may have read a documnet more than once
+    readers_of_document = [] 
     for entry in data:
-        if (entry['subject_doc_id'] == document_uuid):
-            readers_of_document.add(entry['visitor_uuid'])
-    return list(readers_of_document)
+        try:
+            if (entry['subject_doc_id'] == document_uuid):
+                readers_of_document.append(entry['visitor_uuid'])
+        # if subject_doc_id or env_doc_id was missing just pass
+        except Exception:
+            pass
+    return readers_of_document
 # part b
 def get_documents_read_by_user(visitor_uuid):
-    documents_read = set() # set because set will not allow duplicates and a user may have read a documnet more than once
+    documents_read = []
     for entry in data:
-        if (entry['visitor_uuid'] == visitor_uuid):
-            documents_read.add(entry['subject_doc_id'])
-    return list(documents_read)
+        try:
+            if (entry['visitor_uuid'] == visitor_uuid):
+                doc_id = entry['subject_doc_id']
+                if (doc_id):
+                    documents_read.append(doc_id)
+        # an entry may have a missing value and in the case it does just pass
+        except Exception:
+            pass
+    return documents_read
+# get_documents_view_count_by_visitors is a helper function for part c and d
+def get_documents_view_count_by_visitors(readers_list):
+    # make list of all documents 
+    # documents = [i['subject_doc_id'] for i in data] <----- makes KeyError because some values must be missing
+    documents = []
+    for entry in data:
+        try:
+            documents.append(entry['subject_doc_id'])
+        except Exception:
+            pass
+    documents = list(set(documents))
+    # make dictionary of documents with all values -1 to initialize
+    documents_view_counts  = dict([(key, -1) for key in documents])
+    for reader in readers_list:
+        # iterate through the list and for each reader get a list of all the documents they have read
+        documents_read_by_user = get_documents_read_by_user(reader)
+        for document in documents_read_by_user:
+            # iterate through the documents they have read and store the view counts in the dictionary
+            if (documents_view_counts[document] == -1): 
+                documents_view_counts[document] = 1
+            else:
+                documents_view_counts[document] = documents_view_counts[document]+1
+    # return the unsorted dictionary
+    return documents_view_counts
 # part c
-def also_likes():
-    print("TO DO")
-
-
-
+def also_likes(document_uuid, visitor_uuid=None):
+    if (visitor_uuid): # if a visitor uuid has been specified then we only want other documents read by THIS reader
+        # check if visitor_uuid viewed document_uuid
+        visited_docs = get_readers_of_document(visitor_uuid)
+        if (visited_docs.count(document_uuid)>0):
+            readers_of_document = [visitor_uuid]
+        else:
+            raise Exception("Error... Invalid parameters, visitor_uuid did not view document_uuid")
+            print("Error... Invalid parameters, visitor_uuid did not view document_uuid")
+    else:
+        # get a list of all the readers of document_uuid
+        readers_of_document = get_readers_of_document(document_uuid)
+        readers_of_document = list(set(readers_of_document))
+    # use the helper function explained above to get a dictionary of all the documents 
+    # with values of how many times they were read by the specified readers
+    documents_view_counts = get_documents_view_count_by_visitors(readers_of_document)
+    documents_view_counts = list(documents_view_counts.items())
+    print(documents_view_counts)
+    # make a list of only the document uuids 
+    liked_documents_list = [document[0] for document in documents_view_counts if (document[1] != -1)]
+    # coursework spec says "sorted by the sorting function parameter"
+    #liked_documents_list.sort()
+    # make sure document_uuid is not in the list
+    try:
+        liked_documents_list.remove(document_uuid) 
+    except Exception: # if the document is not in this list this will throw ValueError
+        pass
+    # printing stuff to show results in terminal
+    likes_count = len(liked_documents_list)
+    if (likes_count>0):
+        print("\nAlso Likes List: ")
+        for doc_id in liked_documents_list:
+            print(" - "+doc_id)
+        print(liked_documents_list)
+    else:
+        print("\nNo Also Liked Documents Found...")
+    return liked_documents_list
 #part d
-def also_likes_list():
-    also_likes = []
-    # TO DO
-
-
-
-
+def top_10_also_likes(document_uuid, visitor_uuid=None):
+    if (visitor_uuid): # if a visitor uuid has been specified then we only want other documents read by THIS reader
+        # check if visitor_uuid viewed document_uuid
+        visited_docs = get_readers_of_document(visitor_uuid)
+        if (visited_docs.count(document_uuid)>0):
+            readers_of_document = [visitor_uuid]
+        else:
+            raise Exception("Error... Invalid parameters, visitor_uuid did not view document_uuid")
+            print("Error... Invalid parameters, visitor_uuid did not view document_uuid")
+    else:
+        # get a list of all the readers of document_uuid
+        readers_of_document = get_readers_of_document(document_uuid)
+        readers_of_document = list(set(readers_of_document))
+    # use the helper function explained above to get a dictionary of all the documents 
+    # with values of how many times they were read by the specified readers
+    readers_of_document = list(set(readers_of_document))
+    documents_view_counts = get_documents_view_count_by_visitors(readers_of_document)
+    # sort documents from most read to least read
+    sort = sorted(documents_view_counts.items(), key=lambda kv: kv[1], reverse=True)
+    sort = list(sort)
+    # make a list of only the document uuids 
+    liked_documents_list = [document[0] for document in sort if (document[1] != -1)]
+    # make sure document_uuid is not in the list
+    try:
+        liked_documents_list.remove(document_uuid)
+    except Exception: # if the document is not in this list this will throw ValueError
+        pass
+    # get the top 10
+    also_likes = liked_documents_list[:10]
+    no_also_likes = len(also_likes)
+    # printing stuff to show results in terminal
+    if (no_also_likes == 10): 
+        print("\nTop 10 Documents and their Number of Views: ")
+    elif (no_also_likes>0): 
+        print("\nOnly "+str(no_also_likes)+" Also Likes documents were found.")
+        print("Top "+str(no_also_likes)+" Documents and their Number of Views: ")
+    else: # no results
+        print("\nNo Also Liked Documents Found...")
+    i=1
+    for doc in also_likes:
+        print("\nDocument Rank #"+str(i))
+        print("Document with ID:"+str(doc))
+        print("Number of Views: "+str(documents_view_counts[doc]))
+        i = i+1
     return also_likes
 
 # ------------part 6: also likes graph------------
-def show_also_likes_graph():
+def show_also_likes_graph(document_uuid, visitor_uuid):
     print("TO DO")
 
 # ------------Helper Functions------------
+def make_and_show_buttons_also_likes():
+    global E2
+    global E3
+    global L6
+    global gui2
+    document_uuid = E2.get()
+    visitor_uuid = E3.get()
+    if (document_uuid == ""):
+        L6.configure(text="Error... No document_uuid entered, document_uuid is required.")
+        print("Error... No document_uuid entered, document_uuid is required.")
+    else:
+        L6.configure(text="")
+        if (visitor_uuid == ""):
+            visitor_uuid = None
+        B7 = Button(gui2, text ="Print Also Likes List", command=lambda:also_likes(document_uuid, visitor_uuid))
+        B7.pack()
+        B8 = Button(gui2, text ="Print Also Likes Top 10 Documents", command=lambda:top_10_also_likes(document_uuid, visitor_uuid))
+        B8.pack()
+        B9 = Button(gui2, text ="Show Also Likes Graph", command=lambda:show_also_likes_graph(document_uuid, visitor_uuid))
+        B9.pack()
+def open_also_likes_facility():
+    global filename
+    global E2
+    global E3
+    global L6
+    global gui2
+    gui2 = Tk()
+    gui2.title("Also Likes Facility for file: "+filename)
+    gui2.geometry("600x400")
+    L4 = Label(gui2, text="Enter document_uuid:")
+    L4.pack()
+    E2 = Entry(gui2, bd =2)
+    E2.pack()
+    L5 = Label(gui2, text="Enter visitor_uuid (optional):")
+    L5.pack()
+    E3 = Entry(gui2, bd =2)
+    E3.pack()
+    B7 = Button(gui2, text ="Find Also Likes", command=make_and_show_buttons_also_likes)
+    B7.pack()
+    L6 = Label(gui2, text="")
+    L6.pack()
+    gui2.mainloop()
+
 def make_and_show_buttons():
     B2 = Button(gui, text ="Show Views by Country Histogram", command=show_views_by_country_hist)
     B2.pack()
@@ -163,13 +314,14 @@ def make_and_show_buttons():
     B4.pack()
     B5 = Button(gui, text ="Show Reader Profiles Information", command=show_reader_profile_info)
     B5.pack()
-    B6 = Button(gui, text ="Show Also Likes Graph", command=show_also_likes_graph)
+    B6 = Button(gui, text ="Open Also Likes Facility", command=open_also_likes_facility)
     B6.pack()
 
 # ------------file reading stuff------------
 # open and read specified file
 def openfile():
     print("open file button pressed")
+    global filename
     filename = E1.get()
     if (filename == ""):
         L2.configure(text="Error... No file name was entered")
@@ -206,3 +358,14 @@ L2.pack()
 L3 = Label(gui, text="")
 L3.pack()
 gui.mainloop()
+
+# ------------part 8: Command line usage------------
+# TO DO
+
+
+
+
+
+
+
+# --------------------------------------------------
